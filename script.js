9e397efcafb906f8c2c57c0b9b2d55750195475d
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-18 13:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(simpleObject.contains(key)){
+	if(Objects.values(sampleObject).include(key)){
 		return true;
 	}else{
 		return false;
