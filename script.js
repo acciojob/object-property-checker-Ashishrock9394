d76041e11b62512f0464d5506ecd6fbaa88f7446
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-18 13:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(Object.values(sampleObject).includes(key)){
+	if (Object.values(sampleObject).includes(key)){
 		return true;
 	}else{
 		return false;
@@ -12,4 +12,4 @@ function hasKey(key) {
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key));
+// alert(hasKey(key));
